408b026e7e6bfc4ef4501142bbbe71f8644af55d
magical <sun920122@gmail.com>
2026-02-16 04:37:08 +0800

    🚧 測試 tanstack table 套件

--- pages/index.vue
+++ pages/index.vue
@@ -2,117 +2,7 @@
   <div class="container">
     <CourseSearchBar :isSearchPage="true" :resetPage="resetPage" />
     <div class="grid-container">
-      <DataTable
-        ref="scrollTableRef"
-        :value="rowData"
-        :paginator="true"
-        :filters="filters"
-        :global-filter-fields="
-          searchMode.filter_field || defaultGlobalFilterFields
-        "
-        :showGridlines="false"
-        :show-headers="false"
-        :scrollable="true"
-        scrollHeight="calc(100vh - 22rem)"
-        :rows="50"
-        :rowsPerPageOptions="[10, 20, 50, 100]"
-        :loading="loading"
-        contextMenu
-        v-model:contextMenuSelection="selectedProduct"
-        @rowContextmenu="onRowContextMenu"
-        :style="{
-          border: '1px solid var(--p-datatable-border-color)',
-          borderRadius: '0.5rem',
-        }"
-        @page="scroll(-Infinity)"
-      >
-        <template #header>
-          <div class="datatable-header">
-            <span>課程資訊</span>
-            <div class="datatable-header-end">
-              <span>最後更新：{{ currentLastUpdate }}</span>
-              <!-- <Button
-                icon="pi pi-cog"
-                label="顯示設定"
-                variant="text"
-                severity="secondary"
-              ></Button> -->
-            </div>
-          </div>
-        </template>
-
-        <template #loading>
-          <Skeleton
-            width="100%"
-            height="100%"
-            :shape="'rectangle'"
-            :border-radius="'0.5rem'"
-          />
-        </template>
-
-        <template #empty>
-          <div class="flex align-items-center justify-content-center">
-            <i class="pi pi-exclamation-triangle mr-2" />
-            <span>沒有符合條件的課程</span>
-          </div>
-        </template>
-
-        <!-- <template #header> </template> -->
-        <Column :sortable="false">
-          <template #body="{ data }">
-            <CourseCell :course="data" />
-          </template>
-        </Column>
-
-        <!-- Selecte button -->
-        <Column
-          :headerStyle="{
-            width: 'clamp(5rem, 10vw, 10rem)',
-            textAlign: 'center',
-          }"
-          :bodyStyle="{ textAlign: 'center' }"
-          :sortable="false"
-          :style="{
-            width: 'clamp(5rem, 10vw, 10rem)',
-            textAlign: 'center',
-            paddingRight: '1rem',
-          }"
-          :header="''"
-        >
-          <template #body="{ data }">
-            <div
-              style="
-                display: flex;
-                gap: 0.5rem;
-                flex-wrap: wrap;
-                justify-content: center;
-              "
-            >
-              <Button
-                :icon="
-                  checkSelectCode(data.course_code)
-                    ? 'pi pi-heart-fill'
-                    : 'pi pi-heart'
-                "
-                :severity="
-                  checkSelectCode(data.course_code) ? 'danger' : 'secondary'
-                "
-                variant="text"
-                @click="selectCodeHandler(data.course_code, data.course_name)"
-                rounded
-              ></Button>
-              <Button
-                :label="checkSelectedCourse(data.serial_no) ? '取消' : '加入'"
-                :severity="
-                  checkSelectedCourse(data.serial_no) ? 'warn' : 'secondary'
-                "
-                @click="selectCourse(data)"
-              ></Button>
-            </div>
-          </template>
-        </Column>
-      </DataTable>
-      <ContextMenu ref="cm" :model="menuModel" @hide="selectedProduct = null" />
+      <CourseTable />
     </div>
   </div>
 </template>
@@ -127,6 +17,7 @@ import { useSelectCourse } from "~/composables/useSelectCourse";
 
 import { DataTable, Column, Skeleton, Button, ContextMenu } from "primevue";
 import { CourseCell, CourseSearchBar } from "#components";
+import CourseTable from "@/components/CourseTable.vue";
 
 const router = useRouter();
 const route = useRoute();
@@ -168,7 +59,7 @@ const lastUpdate = useState("lastUpdate", () => ({}));
 
 const advancedSearchDisplayValue = useState(
   "advancedSearchDisplayValue",
-  () => "點擊進行進階搜尋"
+  () => "點擊進行進階搜尋",
 );
 const currentlastUpdate = ref("unknown");
 
@@ -233,7 +124,7 @@ const menuModel = ref([
       if (selectedProduct.value) {
         selectCodeHandler(
           selectedProduct.value.course_code,
-          selectedProduct.value.course_name
+          selectedProduct.value.course_name,
         );
       }
       selectedProduct.value = null;
@@ -279,7 +170,7 @@ const filters = useState("filters", () => ({
         operator: FilterOperator.OR,
         constraints: [],
       },
-    ])
+    ]),
   ),
 }));
 
@@ -297,15 +188,15 @@ function scroll(deltaY) {
   }
 
   const scrollable = scrollTableRef.value?.$el.querySelector(
-    ".p-datatable-table-container"
+    ".p-datatable-table-container",
   );
   if (scrollable) {
     scrollable.scrollTop = Math.max(
       0,
       Math.min(
         scrollable.scrollTop + deltaY,
-        scrollable.scrollHeight - scrollable.clientHeight
-      )
+        scrollable.scrollHeight - scrollable.clientHeight,
+      ),
     );
     if (
       scrollable.scrollTop <
