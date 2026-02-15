84b3e7cd036eaac7b3924f4073563b8955f884ca
magical <sun920122@gmail.com>
2026-02-16 04:37:08 +0800

    🚧 測試 tanstack table 套件

---
+++ components/CourseRow.vue
@@ -0,0 +1,221 @@
+<template>
+  <div class="course-row">
+    <div class="course-cell course-code">
+      <span> {{ course.id }}</span>
+      <span>{{ course.course_code }}</span>
+    </div>
+    <div class="course-cell course-title">
+      <span class="course-name">
+        <Button variant="link" size="large" asChild>
+          <NuxtLink
+            :to="{
+              path:
+                '/course/' +
+                `${course.year}/${course.term}/${
+                  course.id
+                }/${encodeURIComponent(course.name)}`,
+            }"
+          >
+            {{ course.name }}
+          </NuxtLink>
+        </Button>
+      </span>
+      <div class="message-group">
+        <Message
+          icon="pi pi-building"
+          severity="secondary"
+          class="custom-message"
+          >{{ course.department }}</Message
+        >
+        <Message
+          icon="pi pi-user"
+          severity="secondary"
+          class="custom-message"
+          >{{ course.teacher }}</Message
+        >
+        <Message
+          v-if="course.time && !course.intensive"
+          icon="pi pi-clock"
+          :severity="
+            course.time.join('/').match(/.* (0|1)([-/\n\r]|$)/)
+              ? 'warn'
+              : 'secondary'
+          "
+          class="custom-message"
+          >{{ course.time.join("/") }}</Message
+        >
+        <Message
+          v-if="course.intensive"
+          icon="pi pi-clock"
+          severity="warn"
+          class="custom-message"
+          style="cursor: pointer"
+          @click="toggle($event)"
+        >
+          <div style="text-decoration: underline">密集課程</div>
+        </Message>
+        <Popover ref="densePopover">
+          <span style="width: 20rem; white-space: nowrap">
+            <div
+              v-for="value in denseData[
+                `${course.course_code}-${course.course_group}`
+              ] || []"
+              :key="value"
+            >
+              {{ value.date }}: {{ value.time_location }}
+            </div>
+          </span>
+        </Popover>
+        <Message
+          icon="pi pi-map-marker"
+          severity="secondary"
+          class="custom-message"
+          v-if="course.location"
+          >{{ course.location }}</Message
+        >
+      </div>
+    </div>
+    <div class="course-cell">
+      <div class="message-group">
+        <Message severity="secondary" class="custom-message"
+          >{{ course.credits }} 學分</Message
+        >
+        <Message severity="secondary" class="custom-message">{{
+          optionMap[course.course_category] || course.course_category
+        }}</Message>
+        <Message
+          v-if="course.general_education"
+          v-for="item in course.general_education.split('/')"
+          :key="item"
+          icon="pi pi-objects-column"
+          severity="secondary"
+          class="custom-message"
+          >{{ generalCoreMap[item] || item }}</Message
+        >
+        <Message
+          v-if="course.credit_program"
+          v-for="item in course.credit_program"
+          :key="item"
+          icon="pi pi-book"
+          severity="secondary"
+          class="custom-message"
+          >{{ item }}</Message
+        >
+        <Message
+          icon="pi pi-users"
+          :severity="course.limit_enrollment > 0 ? 'secondary' : 'warn'"
+          class="custom-message"
+          >{{ `${course.limit_enrollment} 人` }}</Message
+        >
+        <Message
+          v-if="course.english_teaching"
+          severity="error"
+          class="custom-message"
+          >英文授課</Message
+        >
+      </div>
+    </div>
+    <div class="course-cell course-comment">
+      <span v-if="course.restriction">
+        {{
+          course.restriction.replace(/<\/br>/g, "\n").replace(/(?<=.)◎/g, "\n◎")
+        }}
+      </span>
+      <span v-if="course.comment">
+        {{ course.comment.replace(/<\/br>/g, "\n") }}
+      </span>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { Message, Button, Popover } from "primevue";
+
+import type { Course } from "@/composables/useCourseTable";
+
+defineProps({
+  course: {
+    type: Object as () => Course,
+    required: true,
+  },
+});
+
+const optionMap = <Record<string, string>>{
+  必: "必修",
+  選: "選修",
+  通: "通識",
+};
+const generalCoreMap = <Record<string, string>>{
+  A1UG: "人文藝術",
+  A2UG: "社會科學",
+  A3UG: "自然科學",
+  A4UG: "邏輯運算",
+  B1UG: "學院共同課程",
+  B2UG: "跨域專業探索課程",
+  B3UG: "大學入門",
+  C1UG: "專題探究",
+  C2UG: "MOOCs",
+};
+
+const densePopover = ref();
+const denseData = useState("denseData", () => <Record<string, any>>{});
+const toggle = (e: any) => {
+  if (densePopover.value) {
+    densePopover.value.toggle(e);
+  }
+};
+</script>
+
+<style lang="scss">
+.course-row {
+  display: grid;
+  grid-template-columns: 64px 6fr 6fr 7fr auto;
+  align-items: center;
+  padding: 0.5rem;
+  gap: 0.5rem;
+}
+
+.course-cell {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 0.5rem;
+}
+
+@media screen and (max-width: 768px) {
+  .course-row {
+    grid-template-columns: 1fr;
+  }
+
+  .course-cell {
+    flex-direction: row;
+    justify-content: flex-start;
+  }
+
+  .course-title,
+  .course-comment {
+    flex-direction: column;
+  }
+}
+
+.course-name {
+  font-size: medium;
+
+  a {
+    font-weight: bold;
+    color: var(--text-color);
+    padding: 0;
+  }
+}
+
+.message-group {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 0.5rem;
+}
+
+.custom-message {
+  --p-message-content-padding: 0.25rem 0.5rem;
+  --p-message-text-font-size: smaller;
+}
+</style>
